4b4e07ddb42feb9c291ceffdd02d8f51a5bdd039
Izaak White <ww655421@ohio.edu>
2022-09-06 17:43:36 -0400

    Adding c++ file

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Izaak White
+ *        Date: 9/6/2022
+ * Description: Test program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
